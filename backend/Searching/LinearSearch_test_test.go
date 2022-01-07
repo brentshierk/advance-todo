29934b68facf6fabe11b9d691340165ee49f6acf
brent shierk <brentshierk@brents-MacBook-Pro.local>
2022-01-07 15:42:40 -0800

    LinearSearch test done

--- backend/Searching/LinearSearch_test_test.go
+++ backend/Searching/LinearSearch_test_test.go
@@ -1,6 +1,22 @@
 package Searching_test
 
-import "testing"
+import (
+	"backend/Searching"
+	"testing"
+)
 
 func TestLinearsearch(t *testing.T) {
+	arr :=[]int{4,7,8,2,1,99,6,22,10}
+	ans := Searching.Linearsearch(arr,6)
+	if ans != true {
+		t.Errorf("LinearSearch(arr,6)=%d; want true")
+	}
+}
+
+func TestLinearsearch2(t *testing.T) {
+	arr :=[]int{4,7,8,2,1,99,6,22,10}
+	ans := Searching.Linearsearch(arr,33)
+	if ans != false {
+		t.Errorf("LinearSearch(arr,6)=%d; want false")
+	}
 }
