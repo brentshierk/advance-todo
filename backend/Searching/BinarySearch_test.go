a2ec65858996527a4f2597dcd192ef60f862d2a1
brent shierk <brentshierk@brents-MacBook-Pro.local>
2022-01-07 15:35:10 -0800

    Binary search Test done

--- backend/Searching/BinarySearch_test.go
+++ backend/Searching/BinarySearch_test.go
@@ -5,4 +5,17 @@ import (
 )
 
 func TestBinarySearch(t *testing.T) {
+	arr := []int{1,2,3,4,5}
+	ans := BinarySearch(arr,4)
+	if ans != 4{
+		t.Errorf("BinarySearch(arr,4)= %d;want 4",ans)
+	}
+}
+
+func TestBinarySearch2(t *testing.T) {
+	arr := []int{1,2,3,4,5}
+	ans := BinarySearch(arr,6)
+	if ans != -1{
+		t.Errorf("BinarySearch(arr,6)= %d;want -1",ans)
+	}
 }
