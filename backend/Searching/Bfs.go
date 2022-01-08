069d266848a7ee68f1b5bc14b3390c240f887d78
brent shierk <brentshierk@brents-MacBook-Pro.local>
2022-01-07 16:26:09 -0800

    working on bfs/dfs graphs,trees

--- backend/Searching/Bfs.go
+++ backend/Searching/Bfs.go
@@ -1,5 +1,17 @@
 package Searching
 
-func Bfs() {
 
+
+func (n *Node) Bfs(array []int) []int {
+	queue := []*Node{n}
+	for len(queue) > 0 {
+		current := queue[0]
+		queue := queue[1:]
+		array = append(array, current.Value)
+		for _, child := range n.Children {
+			queue := append(queue, child)
+
+		}
+	}
+	return []int{queue[n.Value]}
 }
--- backend/Searching/Dfs.go
+++ backend/Searching/Dfs.go
@@ -1,5 +1,14 @@
 package Searching
 
-func Dfs()  {
-	
+type Node struct {
+	Value    int
+	Children []*Node
+}
+
+func (n *Node) Dfs(array []int) []int {
+	array = append(array, n.Value)
+	for _, child := range n.Children {
+		array = child.Dfs(array)
+	}
+	return array
 }
